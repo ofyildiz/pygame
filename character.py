e790c7f01c55f7048a915384255b256ba13d03cc
Oemer Yildiz <oemer.yildiz@tuhh.de>
2020-10-25 18:39:35 +0100

    restructured AbilityScore class

--- character.py
+++ character.py
@@ -1,7 +1,11 @@
 class AbilityScore:
     def __init__(self,
-                 strength, dexterity, constitution,
-                 intelligence, wisdom, charisma):
+                 strength=0,
+                 dexterity=0,
+                 constitution=0,
+                 intelligence=0,
+                 wisdom=0,
+                 charisma=0):
         self.strength = strength
         self.dexterity = dexterity
         self.constitution = constitution
@@ -9,54 +13,79 @@ class AbilityScore:
         self.wisdom = wisdom
         self.charisma = charisma
 
-class AbilityModifier:
-    def __init__(self,
-                 strength, dexterity, constitution,
-                 intelligence, wisdom, charisma):
-        self.strength = self.get_modifier(strength)
-        self.dexterity = self.get_modifier(dexterity)
-        self.constitution = self.get_modifier(constitution)
-        self.intelligence = self.get_modifier(intelligence)
-        self.wisdom = self.get_modifier(wisdom)
-        self.charisma = self.get_modifier(charisma)
+    def __add__(self, other):
+        strength = self.strength + other.strength
+        dexterity = self.dexterity + other.dexterity
+        constitution = self.constitution + other.constitution
+        intelligence = self.intelligence + other.intelligence
+        wisdom = self.wisdom + other.wisdom
+        charisma = self.charisma + other.charisma
+        abilityscore = AbilityScore(strength,
+                                    dexterity,
+                                    constitution,
+                                    intelligence,
+                                    wisdom,
+                                    charisma)
+        return abilityscore
 
-    def get_modifier(self, score):
-        modifier = score//2 - 5
+    def return_modifier(self):
+        mod = lambda score: score//2 - 5
+        strength = mod(self.strength)
+        dexterity = mod(self.dexterity)
+        constitution = mod(self.constitution)
+        intelligence = mod(self.intelligence)
+        wisdom = mod(self.wisdom)
+        charisma = mod(self.charisma)
+        modifier = AbilityScore(strength,
+                                dexterity,
+                                constitution,
+                                intelligence,
+                                wisdom,
+                                charisma)
         return modifier
 
-class Character():
+class Race:
     def __init__(self,
-                 strength, dexterity, constitution,
-                 intelligence, wisdom, charisma,
                  race):
-        self.abilityscore = AbilityScore(strength, dexterity, constitution,
-                                    intelligence, wisdom, charisma)
-        self.abilitymodifier = None
-        self.speed = None
         self.race = race
+        self.speed = 0
+        self.abilityscore = AbilityScore()
         self.set_traits()
 
     def set_traits(self):
-        if self.race == 'Dwarf':
-            self.abilityscore.constitution += 2
-            self.speed = 25
-        elif self.race == 'Elf':
-            self.abilityscore.dexterity += 2
-            self.speed = 30
-        elif self.race == 'Halfling':
-            self.abilityscore.dexterity += 2
-            self.speed = 25
-        elif self.race == 'Human':
+        if self.race == 'Human':
             self.abilityscore.strength += 1
             self.abilityscore.dexterity += 1
             self.abilityscore.constitution += 1
             self.abilityscore.intelligence += 1
             self.abilityscore.wisdom += 1
             self.abilityscore.charisma += 1
-            self.speed = 30
-            self.abilitymodifier = AbilityModifier(self.abilityscore.strength,
-                                                   self.abilityscore.dexterity,
-                                                   self.abilityscore.constitution,
-                                                   self.abilityscore.intelligence,
-                                                   self.abilityscore.wisdom,
-                                                   self.abilityscore.charisma)
+            self.speed += 30
+        elif self.race == 'Elf':
+            self.abilityscore.dexterity += 2
+        elif self.race == 'Dwarf':
+            self.abilityscore.constitution += 2
+            self.speed += 25
+            self.speed += 30
+        elif self.race == 'Halfling':
+            self.abilityscore.dexterity += 2
+            self.speed += 25
+
+class Character():
+    def __init__(self,
+                 strength=10,
+                 dexterity=10,
+                 constitution=10,
+                 intelligence=10,
+                 wisdom=10,
+                 charisma=10,
+                 race='Human'):
+        self.abilityscore_base = AbilityScore(strength,
+                                              dexterity,
+                                              constitution,
+                                              intelligence,
+                                              wisdom,
+                                              charisma)
+        self.race = Race(race)
+        self.abilityscore = self.abilityscore_base + self.race.abilityscore
+        self.abilitymodifier = self.abilityscore.return_modifier()
