597b109a5a76fbf4ea89f4f50677d61bc008913e
Oemer Yildiz <oemer.yildiz@tuhh.de>
2020-10-29 12:07:53 +0100

    added levelup to character class

--- character.py
+++ character.py
@@ -2,21 +2,12 @@ import random as rd
 
 class Ability:
     def __init__(self, base=0, temp=0):
-        self.base, self.temp, self.score, self.mod = base, temp, 0, 0
-        self.update()
+        self.base, self.temp, self.score, self.mod = 0, 0, 0, 0
 
     def update(self, temp=0):
         self.temp = temp
         self.score = self.base + self.temp
         self.mod = (self.score)//2 - 5
-
-class Dice:
-    def __init__(self, typ=20):
-        self.typ = typ
-        self.result = typ
-
-    def roll(self):
-        self.result = rd.randint(1, self.typ)
 
 class Stats:
     def __init__(self, str=0, dex=0, con=0, int=0, wis=0, cha=0):
@@ -36,17 +27,28 @@ class Race:
     def __init__(self, race='Human'):
         self.race = race
         if self.race == 'Human':
-            self.statlist = [1, 1, 1, 1, 1, 1]
+            self.statboni = [1, 1, 1, 1, 1, 1]
             self.speed = 30
         elif self.race == 'Elf':
-            self.statlist = [0, 2, 0, 0, 0, 0]
+            self.statboni = [0, 2, 0, 0, 0, 0]
             self.speed = 30
         elif self.race == 'Dwarf':
-            self.statlist = [0, 0, 2, 0, 0, 0]
+            self.statboni = [0, 0, 2, 0, 0, 0]
             self.speed = 25
         elif self.race == 'Halfling':
-            self.statlist = [0, 2, 0, 0, 0, 0]
+            self.statboni = [0, 2, 0, 0, 0, 0]
             self.speed = 25
+        else:
+            self.statboni = [0, 0, 0, 0, 0, 0]
+            self.speed = 25
+
+class Dice:
+    def __init__(self, typ=20):
+        self.typ = typ
+        self.result = typ
+
+    def roll(self):
+        self.result = rd.randint(1, self.typ)
 
 class Job:
     def __init__(self, job='Barbarian'):
@@ -64,5 +66,14 @@ class Character():
                  race='Human', job='Barbarian'):
         self.stats = Stats(str, dex, con, int, wis, cha)
         self.race = Race(race)
-        self.stats.update([self.race.statlist])
+        self.stats.update([self.race.statboni])
         self.job = Job(job)
+        self.level = 1
+        self.exp = 0
+        self.hitpoints = self.job.hitdie[0].typ + self.stats.con.mod
+
+    def levelup(self):
+        self.level += 1
+        self.job.levelup()
+        self.job.hitdie[-1].roll
+        self.hitpoints += self.job.hitdie[-1].result + self.stats.con.mod
