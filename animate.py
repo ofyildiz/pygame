b942bff5b84d4c36878836b9cdbd268c6466a550
goofy <oemer.yildiz@tuhh.de>
2020-12-14 12:07:03 +0100

    ability to draw background from csv (#6)

--- animate.py
+++ animate.py
@@ -1,8 +1,9 @@
+import csv
 import pygame
 from pygame.locals import *
 
 class Animation:
-    def __init__(self, filepath, rows=1, cols=1, pos=0, num=1, x=0, y=0, vel=5, w=640, h=400):
+    def __init__(self, filepath, rows=1, cols=1, pos=0, num=1, x=0, y=0, vel=16, w=640, h=400):
         """
         This class handles animation, meaning sprites or collection of sprites.
         The input:
@@ -21,6 +22,7 @@ class Animation:
         The output:
             sprites - (list) list of unique sprites
         """
+        self.num = num
         self.x = x
         self.y = y
         self.w = w
@@ -38,30 +40,42 @@ class Animation:
             self.sprites.append(image.subsurface(image.get_clip()))
 
         self.imidx = 0
-        self.imidy = 0
+        self.posidx = [self.x//self.charwidth, self.y//self.charheight]
 
-    def update(self, display):
-        if pygame.key.get_pressed()[pygame.K_RIGHT] and self.x + self.vel <= self.w - self.charwidth:
+    def update(self, display, background):
+        if pygame.key.get_pressed()[pygame.K_RIGHT] and background.array[self.posidx[1]][self.posidx[0]+1] != 'w':
             self.x += self.vel
-        elif pygame.key.get_pressed()[pygame.K_LEFT] and self.x - self.vel >= 0:
+            self.posidx[0] += 1
+            pygame.time.wait(200)
+        elif pygame.key.get_pressed()[pygame.K_LEFT] and background.array[self.posidx[1]][self.posidx[0]-1] != 'w':
             self.x -= self.vel
-        elif pygame.key.get_pressed()[pygame.K_UP] and self.y - self.vel >=0:
+            self.posidx[0] -= 1
+            pygame.time.wait(200)
+        elif pygame.key.get_pressed()[pygame.K_UP] and background.array[self.posidx[1]-1][self.posidx[0]] != 'w':
             self.y -= self.vel
-        elif pygame.key.get_pressed()[pygame.K_DOWN] and self.y + self.vel <= self.h - self.charheight:
+            self.posidx[1] -= 1
+            pygame.time.wait(200)
+        elif pygame.key.get_pressed()[pygame.K_DOWN] and background.array[self.posidx[1]+1][self.posidx[0]] != 'w':
             self.y += self.vel
-        display.blit(self.sprites[self.imidy], (self.x, self.y))
+            self.posidx[1] += 1
+            pygame.time.wait(200)
+        display.blit(self.sprites[self.imidx//(60//self.num)], (self.x, self.y))
         if self.imidx+1 == 60:
             self.imidx = 0
-            self.imidy = 0
-        elif 0 <= self.imidx+1 < 15:
+        else:
             self.imidx += 1
-            self.imidy = 0
-        elif 15 <= self.imidx+1 < 30:
-            self.imidx += 1
-            self.imidy = 1
-        elif 30 <= self.imidx+1 < 45:
-            self.imidx += 1
-            self.imidy = 2
-        elif 45 <= self.imidx+1 < 60:
-            self.imidx += 1
-            self.imidy = 3
+
+class Background:
+    def __init__(self, filepath, w=16, h=16):
+        self.w = w
+        self.h = h
+        with open(filepath) as csvfile:
+            self.array = list(csv.reader(csvfile, delimiter=' '))
+
+    def update(self, display):
+        for rowKey, rowElem in enumerate(self.array):
+            for colKey, colElem in enumerate(rowElem):
+                if colElem == 'w':
+                    pygame.draw.rect(display, pygame.color.Color('blue'), pygame.Rect(colKey*self.w, rowKey*self.h, self.w, self.h))
+                elif colElem == 'g':
+                    pygame.draw.rect(display, pygame.color.Color('green'), pygame.Rect(colKey*self.w, rowKey*self.h, self.w, self.h))
